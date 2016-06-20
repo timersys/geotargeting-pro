93a60c4c495f4b9c9b3fa23f3b499bb5b1d35066
Damian Logghe <damian@timersys.com>
2016-06-20 11:58:21 -0300

    vc fully working

--- includes/class-geotarget-vc.php
+++ includes/class-geotarget-vc.php
@@ -81,49 +81,151 @@ class GeoTarget_VC {
 
 		More info: http://kb.wpbakery.com/index.php?title=Vc_map
 		*/
-		vc_map_update( 'vc_row', array(
-			"params" => array(
-				array(
-					"type" => "textfield",
-					"holder" => "div",
-					"class" => "",
-					"heading" => __("Country", 'geot'),
-					"param_name" => "country",
-					"value" => __("", 'geot'),
-					"description" => __("Type country name or ISO code. Also you can write a comma separated list of countries", 'geot'),
-					'group' => __( 'GeoTargeting', 'geot' ),
-				),
-				array(
-					"type" => "dropdown",
-					"holder" => "div",
-					"class" => "",
-					"heading" => __("Region", 'geot'),
-					"param_name" => "region",
-					"value" => $dropdown_values,
-					"description" => __("Choose region name to show content to", 'geot'),
-					'group' => __( 'GeoTargeting', 'geot' ),
-				),
-				array(
-					"type" => "textfield",
-					"holder" => "div",
-					"class" => "",
-					"heading" => __("Exclude Country", 'geot'),
-					"param_name" => "exclude_country",
-					"value" => __("", 'geot'),
-					"description" => __("Type country name or ISO code. Also you could write a comma separated list of countries", 'geot'),
-					'group' => __( 'GeoTargeting', 'geot' ),
-				),
-				array(
-					"type" => "dropdown",
-					"holder" => "div",
-					"class" => "",
-					"heading" => __("Exclude Region", 'geot'),
-					"param_name" => "exclude_region",
-					"value" => $dropdown_values,
-					"description" => __("Choose region name to exclude content.", 'geot'),
-					'group' => __( 'GeoTargeting', 'geot' ),
-				),
+		vc_map(
+			array(
+				'name'                      => __( 'Target Countries' , 'geot' ),
+				'is_container'              => true,
+				'content_element'           => true,
+				'base'                      => 'vc_geot',
+				'icon'                      => GEOT_PLUGIN_URL . '/admin/img/world.png',
+				'show_settings_on_create'   => true,
+				'category'                  => __( 'Geotargeting', 'geot' ),
+				'description'               => __( 'Place elements inside this geot container', 'geot' ),
+				'html_template'             => GEOT_PLUGIN_DIR . '/includes/vc/vc_geot.php',
+				"params"                    => array(
+					array(
+						"type" => "textfield",
+						"class" => "",
+						"heading" => __("Country", 'geot'),
+						"param_name" => "country",
+						"value" => __("", 'geot'),
+						"description" => __("Type country name or ISO code. Also you can write a comma separated list of countries", 'geot'),
+						'group' => __( 'GeoTargeting', 'geot' ),
+					),
+					array(
+						"type" => "dropdown",
+						"class" => "",
+						"heading" => __("Region", 'geot'),
+						"param_name" => "region",
+						"value" => $dropdown_values,
+						"description" => __("Choose region name to show content to", 'geot'),
+						'group' => __( 'GeoTargeting', 'geot' ),
+					),
+					array(
+						"type" => "textfield",
+						"class" => "",
+						"heading" => __("Exclude Country", 'geot'),
+						"param_name" => "exclude_country",
+						"value" => __("", 'geot'),
+						"description" => __("Type country name or ISO code. Also you could write a comma separated list of countries", 'geot'),
+						'group' => __( 'GeoTargeting', 'geot' ),
+					),
+					array(
+						"type" => "dropdown",
+						"class" => "",
+						"heading" => __("Exclude Region", 'geot'),
+						"param_name" => "exclude_region",
+						"value" => $dropdown_values,
+						"description" => __("Choose region name to exclude content.", 'geot'),
+						'group' => __( 'GeoTargeting', 'geot' ),
+					),
+				)
 			)
-		) );
+		);
+		vc_map(
+			array(
+				'name'                      => __( 'Target Cities' , 'geot' ),
+				'is_container'              => true,
+				'content_element'           => true,
+				'base'                      => 'vc_geot_city',
+				'icon'                      => GEOT_PLUGIN_URL . '/admin/img/cities.png',
+				'show_settings_on_create'   => true,
+				'category'                  => __( 'Geotargeting', 'geot' ),
+				'description'               => __( 'Place elements inside this geot container', 'geot' ),
+				'html_template'             => GEOT_PLUGIN_DIR . '/includes/vc/vc_geot_city.php',
+				"params"                    => array(
+					array(
+						"type" => "textfield",
+						"class" => "",
+						"heading" => __("City", 'geot'),
+						"param_name" => "city",
+						"value" => __("", 'geot'),
+						"description" => __("Type city name. Also you can write a comma separated list of cities", 'geot'),
+						'group' => __( 'GeoTargeting', 'geot' ),
+					),
+					array(
+						"type" => "dropdown",
+						"class" => "",
+						"heading" => __("City Region", 'geot'),
+						"param_name" => "region",
+						"value" => $dropdown_values,
+						"description" => __("Choose region name to show content to", 'geot'),
+						'group' => __( 'GeoTargeting', 'geot' ),
+					),
+					array(
+						"type" => "textfield",
+						"class" => "",
+						"heading" => __("Exclude City", 'geot'),
+						"param_name" => "exclude_city",
+						"value" => __("", 'geot'),
+						"description" => __("Type city name. Also you could write a comma separated list of cities", 'geot'),
+						'group' => __( 'GeoTargeting', 'geot' ),
+					),
+					array(
+						"type" => "dropdown",
+						"class" => "",
+						"heading" => __("Exclude City Region", 'geot'),
+						"param_name" => "exclude_region",
+						"value" => $dropdown_values,
+						"description" => __("Choose region name to exclude content.", 'geot'),
+						'group' => __( 'GeoTargeting', 'geot' ),
+					),
+				)
+			)
+		);
+		vc_map(
+			array(
+				'name'                      => __( 'Target States' , 'geot' ),
+				'is_container'              => true,
+				'content_element'           => true,
+				'base'                      => 'vc_geot_state',
+				'icon'                      => GEOT_PLUGIN_URL . '/admin/img/states.png',
+				'show_settings_on_create'   => true,
+				'category'                  => __( 'Geotargeting', 'geot' ),
+				'description'               => __( 'Place elements inside this geot container', 'geot' ),
+				'html_template'             => GEOT_PLUGIN_DIR . '/includes/vc/vc_geot_state.php',
+				"params"                    => array(
+					array(
+						"type" => "textfield",
+						"class" => "",
+						"heading" => __("State", 'geot'),
+						"param_name" => "state",
+						"value" => __("", 'geot'),
+						"description" => __("Type state name or ISO code. Also you can write a comma separated list of states", 'geot'),
+						'group' => __( 'GeoTargeting', 'geot' ),
+					),
+					array(
+						"type" => "textfield",
+						"class" => "",
+						"heading" => __("Exclude State", 'geot'),
+						"param_name" => "exclude_state",
+						"value" => __("", 'geot'),
+						"description" => __("Type state name or ISO code. Also you can write a comma separated list of states", 'geot'),
+						'group' => __( 'GeoTargeting', 'geot' ),
+					)
+				)
+			)
+		);
+
 	}
 }
+add_action('init',function(){
+	if ( class_exists( 'WPBakeryShortCodesContainer' ) ) {
+		class WPBakeryShortCode_VC_Geot extends WPBakeryShortCodesContainer {
+		}
+		class WPBakeryShortCode_VC_Geot_City extends WPBakeryShortCodesContainer {
+		}
+		class WPBakeryShortCode_VC_Geot_State extends WPBakeryShortCodesContainer {
+		}
+	}
+});
---
+++ includes/vc/vc_geot_city.php
@@ -0,0 +1,26 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
+/**
+ * Shortcode attributes
+ * @var $city
+ * @var $exclude_city
+ * @var $region
+ * @var $exclude_region
+ * @var $this WPBakeryShortCode_VC_Geot
+ */
+$city = $exclude_city = $region = $exclude_region = '';
+$atts = vc_map_get_attributes( $this->getShortcode(), $atts );
+extract( $atts );
+
+$opts = apply_filters('geot/settings_page/opts', get_option( 'geot_settings' ) );
+
+if( isset( $opts['ajax_mode'] ) && $opts['ajax_mode'] == '1' ) {
+	echo '<div class="geot-ajax geot-filter" data-action="city_filter" data-filter="' . $city . '" data-region="' . $region . '" data-ex_filter="' . $exclude_city . '" data-ex_region="' . $exclude_region . '">' . wpb_js_remove_wpautop( $content ) . '</div>';
+} else {
+	if ( geot_target_city( $city, $region, $exclude_city, $exclude_region ) ) {
+		echo wpb_js_remove_wpautop( $content );
+	}
+}
