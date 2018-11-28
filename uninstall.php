ac935e0ad0ffe7633e47ea4ebf8e8ffec46f0190
Damian <info@timersys.com>
2018-11-28 14:14:51 -0300

    uninstall fix

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Fired when the plugin is uninstalled.
  *
@@ -12,8 +11,21 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 $opts = get_option('geot_settings');
 
-// only run If last plugin being uninstalled
+// only uninstal regions/settings  If last plugin being uninstalled
 if( isset( $opts['geot_uninstall']) && '1' == $opts['geot_uninstall'] && ! function_exists('geot') ) {
-	require plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
-	GeotFunctions\uninstall();
+	require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+	GeotFunctions\geot_uninstall();
+}
+// Run if this plugin is being uninstalled and uninstall is checked
+if( isset( $opts['geot_uninstall']) && '1' == $opts['geot_uninstall']  ) {
+	require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+	try {
+		//_delete all meta data
+		$wpdb->query( "DELETE FROM $wpdb->postmeta WHERE meta_key = '_menu_item_geot' OR meta_key = 'geot_options' OR meta_key = '_geot_post'" );
+	} catch (Exception $e ){
+	}
+	// uncheck geot uninstall just in case after the plugin it's removed
+	$opts = get_option('geot_settings');
+	$opts['uninstall'] = '';
+	update_option( 'geot_settings', $opts);
 }
